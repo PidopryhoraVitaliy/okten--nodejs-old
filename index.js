cd743accf542ab5211efe38c1bb4d1d91635cb68
Pidopryhora Vitaliy <vpidopryhora@gmail.com>
2026-09-15 10:57:58 +0300

    Init

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        data: 'Hello World!',
+    }));
+});
+
+// // Create a local server to receive data from
+// const server = http.createServer();
+//
+// // Listen to the request event
+// server.on('request', (request, res) => {
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//     res.end(JSON.stringify({
+//         data: 'test node server!',
+//     }));
+// });
+
+server.listen(8000);
