81ef9a02283d5486bcc20138c02c7f4e544a7eff
Pidopryhora Vitaliy <vpidopryhora@gmail.com>
2026-09-15 15:10:05 +0300

    hw2 - files creator + log

--- index.js
+++ index.js
@@ -9,15 +9,21 @@ const fs = require('node:fs/promises');
 const creator = async () => {
     const basePath = path.join(__dirname, 'baseFolder');
     await fs.mkdir(basePath, { recursive: true });
+    const baseFolderStat = await fs.stat(basePath);
+    console.log(`Creating folder ${basePath}`, `| isDirectory: ${baseFolderStat.isDirectory()} | isFile: ${baseFolderStat.isFile()}`);
 
     for (let folderCounter = 1; folderCounter <= 5; folderCounter++) {
         const folderPath = path.join(basePath, `folder-${folderCounter.toString()}`);
         await fs.mkdir(folderPath, {recursive: true});
+        const folderStat = await fs.stat(folderPath);
+        console.log(`Creating folder ${folderPath}`, `| isDirectory: ${folderStat.isDirectory()} | isFile: ${folderStat.isFile()}`);
 
         for (let fileCount = 1; fileCount <= 5; fileCount++) {
             const data = `some data!\nfolder-${folderCounter.toString()}\nfile-${fileCount.toString()}`;
             const pathToFile = path.join(folderPath, `file-${fileCount.toString()}.txt`);
             await fs.writeFile(pathToFile, data, {encoding: 'utf8'});
+            const stat = await fs.stat(pathToFile);
+            console.log(`Creating file ${pathToFile}`, `| isDirectory: ${stat.isDirectory()} | isFile: ${stat.isFile()}`);
         }
     }
 }
