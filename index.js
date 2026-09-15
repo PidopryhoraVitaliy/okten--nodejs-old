ca902cf1641d29d9938677f4a42c51a2082853ad
Pidopryhora Vitaliy <vpidopryhora@gmail.com>
2026-09-15 11:03:14 +0300

    hw1 - server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('node:http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({
-        data: 'Hello World!',
+        data: 'Hello World (by node server)!',
     }));
 });
 
